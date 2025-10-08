26da3388fb5b95f3ecd6a8de70824a5873572c86
R.M.SACHITHRA SUVIMAL <109018944+sachithrasuvimal@users.noreply.github.com>
2025-10-09 00:06:50 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:workout_planner/pages/add_new_page.dart';
+import 'package:workout_planner/pages/favourites.dart';
+import 'package:workout_planner/pages/home_page.dart';
+import 'package:workout_planner/pages/profile_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // 1. Declare and initialize the _currentIndex variable
+  int _currentIndex = 0; 
+
+  final List<Widget> _pages = [
+    HomePage(),
+    AddNewPage(),
+    FavouritesPage(),
+    ProfilePage(),
+  ];
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Workout Planner",
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.lightBlue,
+          // Now _currentIndex is a valid variable
+          currentIndex: _currentIndex, 
+          onTap: (index){
+            setState(() {
+              // Now _currentIndex can be updated
+              _currentIndex = index; 
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add New"),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favourites"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          ],
+        ),
+        // Now _currentIndex is used to select the page
+        body: _pages[_currentIndex], 
+      )
+    );
+  }
+}
---
+++ lib/pages/add_new_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AddNewPage extends StatefulWidget {
+  const AddNewPage({super.key});
+
+  @override
+  State<AddNewPage> createState() => _AddNewPageState();
+}
+
+class _AddNewPageState extends State<AddNewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: SafeArea(
+       child: SingleChildScrollView(
+        child: Center(
+          child: Text(
+            "add New page"
+          ),
+        ),
+       ),
+     )
+    );
+    
+  }
+}
