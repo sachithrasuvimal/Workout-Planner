26ad3b48c9abfe8aa3f030991057d3b3de4b1cfa
R.M.SACHITHRA SUVIMAL <109018944+sachithrasuvimal@users.noreply.github.com>
2025-10-10 00:08:56 +0530

    new color and responsive

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kSubTitleColor = Colors.grey;
+const kMainWhite = Colors.white;
+const kMainBlack = Colors.black;
+const lMainColor = Colors.lightBlue;
+
+const kMainDarkBlue = Color(0xff0147fc);
+const kMainPinkColor = Color(0xffFF0000);
+
+const kMainLightBlueColor = Color(0xff00f0FF);
+const kMainpurpleColoer = Color(0xffEB00FF);
+const kMainPurpleColor = Color(0xffFF006B);
