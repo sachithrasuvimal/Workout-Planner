6459115edb4efbb34c1f6fd205c17cd89fafb716
R.M.SACHITHRA SUVIMAL <109018944+sachithrasuvimal@users.noreply.github.com>
2025-10-10 00:10:44 +0530

    home page dat and time add

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:workout_planner/constants/colors.dart';
+import 'package:workout_planner/constants/responsive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,16 +11,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+//Date and time
+  final DateFormat formatter = DateFormat('EEEE-MMMM');
+  final DateFormat dayFormat = DateFormat('dd');
+
+  //User Name
+  final userData
+
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    String formattedDate = formatter.format(now);
+    String formatterDay = dayFormat.format(now);
+
+
     return Scaffold(
      body: SafeArea(
        child: SingleChildScrollView(
-        child: Center(
-          child: Text(
-            "home page"
-          ),
+        child: Padding
+        (padding: const EdgeInsets.all(kDefaultPadding),
+        child: Column(
+          children: [
+            Text(
+              "$formattedDate $formatterDay",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: kSubTitleColor
+              ),
+            ),
+          ],
         ),
+       ),
        ),
      )
     );
